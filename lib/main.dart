c1d815a92731e9d2eb030d7dc4de62297472b32c
AbuBakarSiddikNayem <md.abubakarsiddiknayem@gmail.com>
2025-07-11 17:44:06 +0600

    Fix home page title to include 'App' for consistency

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cloth App',
-      home: const MyHomePage(title: 'Clothings'),
+      home: const MyHomePage(title: 'Clothings App'),
     );
   }
 }
